a28f2c19ff18e31a907d9ebfd1dae26cb9961997
Administrator <Administrator@192.168.222.1>
2017-09-30 09:51:18 -0500

    updating Dunkin Project by srini- 09/30

---
+++ DunkinProject/src/test/java/LoginPackage/Login.java
@@ -0,0 +1,15 @@
+package LoginPackage;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+
+	}
+
+}
